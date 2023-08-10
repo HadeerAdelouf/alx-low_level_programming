87f8248f23ce8175a3107df4a02fc2db7aaf8b9c
HadeerAdelouf <hadiradel999@gmail.com>
2023-08-10 13:38:24 -0700

    my second commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
-puts ("\"Programming is like building a multilingual puzzle");    
+/**
+ * main _ entry point
+ * description print a pherase by using puts
+ *Return: 0 if exited properly, non-zero otherwise
+ */
 
-    return 0;
+int main(void) {
+      puts ("\"Programming is like building a multilingual puzzle");    
+
+return 0;
 }
