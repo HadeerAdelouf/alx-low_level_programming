c83d876b1909867ab71aac7b8612b024b258f6ef
HadeerAdelouf <hadiradel999@gmail.com>
2023-08-10 14:22:34 -0700

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 
 /**
  *main - prints phrase using printf function
- *return : 0 if exited properly, non zero otherwise
+ *Return: 0 if exited properly, non zero otherwise
  */
-int main(void) 
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
