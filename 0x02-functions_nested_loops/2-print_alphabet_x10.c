7fd31ed973985e74a2ba4663c21c9f8f5bee0fe7
HadeerAdelouf <hadiradel999@gmail.com>
2023-08-15 12:50:50 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times from a to z
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+	int line;
+	int ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
