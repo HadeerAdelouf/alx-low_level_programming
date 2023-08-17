c5cf7cfef094f3d0e9e646f6a5d1fdc9dd6ae15f
HadeerAdelouf <hadiradel999@gmail.com>
2023-08-17 16:18:06 -0700

    5

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * int _isdigit - checks for a digit (0 to 9)
+ *_isdigit - checks for a digit (0 to 9)
  *
  * @c: input number
- * Return 1 if c is a digit 0 othrwise
+ *Return: 1 if c is a digit 0 othrwise
  */
 
 int _isdigit(int c)
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*void more_numbers - function printfrom 0 to 14
+*10 times
+*return: void
+ */
+
+void more_numbers(void)
+{
+int num, i, count;
+
+	for (i = 0 ; i <= 10; i++)
+	{
+		for (count = 0; count <= 14 ; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
