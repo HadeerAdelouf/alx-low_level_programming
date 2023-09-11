078cf217af8686131da346828e32a74f240b2ba2
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-11 12:42:40 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include<stdio.h>
 /**
  * struct dog - struct for info about dogs
  * @name:name of the dog
@@ -10,7 +9,7 @@
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
 #endif
