a01b8a7b387d886447a13c0a3abc0afa89a8256d
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-13 15:48:18 -0700

    5

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*fptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	fptr = get_op_func(argv[2]);
+
+	if (fptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", fptr(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include"3-calc.h"
+/**
+ * op_add- adds two intgers
+ * @a:first intger
+ * @b:second intger
+ * Return:sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub-subtracts two intgers
+ * @a:first intger
+ * @b:second intger
+ * Return:sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- multiply two intgers
+ * @a:first intger
+ * @b:second intger
+ * Return:the product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- divides two intgers
+ * @a:first intger
+ * @b:second intger
+ * Return:division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- modulo two intgers
+ * @a:first intger
+ * @b:second intger
+ * Return:remainder
+ *
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
