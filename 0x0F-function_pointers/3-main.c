c5368554176f6b54d5cd56654b34ac11eb53d071
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-13 16:01:10 -0700

    5

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int a, b, c;
 	int (*fptr)(int, int);
 
 	if (argc != 4)
@@ -18,23 +18,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	fptr = get_op_func(argv[2]);
-
 	if (fptr == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", fptr(a, b));
+	c = fptr(a, b);
+	printf("%d\n", c);
 	return (0);
 }
