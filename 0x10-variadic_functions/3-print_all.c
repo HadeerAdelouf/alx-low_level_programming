d8b4b6f5fa6e2cb4b9f4f4a3ab195a84d28b1616
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-14 15:26:39 -0700

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ * Return:void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	int i = 0;
+	char c;
+	char *s;
+	float f;
+	
+	while (format && format[i])
+    {
+	    c = format[i];
+	    if (c == 'c')
+	    {
+		    printf("%c", va_arg(args, int));
+	    }
+	    else if (c == 'i')
+	    {
+		    printf("%d", va_arg(args, int));
+	    }
+	    else if (c == 'f')
+	    {
+		    printf("%f", va_arg(args, double));
+	    }
+	    else if (c == 's')
+	    {
+		    s = va_arg(args, char *);
+		    if (s == NULL)
+		    {
+			    printf("(nil)");
+		    }
+		    else
+			    printf("%s", s);
+	    }
+	    i++;
+	if (format[i]) 
+	{
+		printf(", ");
+	}
+	va_end(args);
+	printf("\n");
+}
