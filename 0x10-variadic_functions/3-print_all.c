6391295754da4544de99a4fb956c395cc40563c8
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-14 15:31:52 -0700

    4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,43 +7,41 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-
-	va_start(args, format);
 	int i = 0;
 	char c;
 	char *s;
 	float f;
-	
+
+	va_start(args, format);
 	while (format && format[i])
-    {
-	    c = format[i];
-	    if (c == 'c')
-	    {
-		    printf("%c", va_arg(args, int));
-	    }
-	    else if (c == 'i')
-	    {
-		    printf("%d", va_arg(args, int));
-	    }
-	    else if (c == 'f')
-	    {
-		    printf("%f", va_arg(args, double));
-	    }
-	    else if (c == 's')
-	    {
-		    s = va_arg(args, char *);
-		    if (s == NULL)
-		    {
-			    printf("(nil)");
-		    }
-		    else
-			    printf("%s", s);
-	    }
-	    i++;
-	if (format[i]) 
 	{
-		printf(", ");
+		c = format[i];
+
+		if (c == 'c')
+			printf("%c", va_arg(args, int));
+		else if (c == 'i')
+		{
+			printf("%d", va_arg(args, int));
+		}
+		else if (c == 'f')
+		{
+			printf("%f", va_arg(args, double));
+		}
+		else if (c == 's')
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+		}
+
+		i++;
+
+		if (format[i])
+			printf(", ");
 	}
+
 	va_end(args);
 	printf("\n");
 }
