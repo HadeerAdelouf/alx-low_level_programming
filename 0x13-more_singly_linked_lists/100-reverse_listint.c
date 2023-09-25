61dbbaffbb27aebb8ca44440db36c04f5699f49a
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-25 14:22:24 -0700

    adv 1

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+/**
+ * reverse_listint - reverses linked list
+ * @head:double pointer to first node
+ * Return:pointer to first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	head = prev;
+	return (*head);
+}
