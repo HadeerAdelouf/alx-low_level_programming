87e2510d8ef887e19d9398ea166b3e41953cb39e
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-25 03:56:51 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees listint_t
+ * @head: double pointer to the head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *node, *temp;
+
+	if (!head)
+		return;
+
+	node = *head;
+	while (node)
+	{
+		temp = node;
+		node = node->next;
+		free(temp);
+	}
+	*head = NULL;
+}
