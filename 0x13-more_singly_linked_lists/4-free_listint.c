df24af253b8350caf2c0de3b38350f637e316837
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-25 03:38:50 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*free_listint - frees a linkedList
+*@head:pointer to  head of the list
+*Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
