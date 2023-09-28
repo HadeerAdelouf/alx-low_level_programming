88a08351f2441f6ce65ca1845e8958f982f31164
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-28 12:17:38 -0700

    task 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary repres of a long number
+ * @n: unsigned long int
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	int shift = sizeof(unsigned long int) * 8 - 1;
+	int flag = 0;
+
+	while (shift >= 0)
+	{
+		unsigned long int bit = n >> shift;
+
+		if (bit & 1)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag || shift == 0)
+		{
+			_putchar('0');
+		}
+		shift--;
+	}
+}
