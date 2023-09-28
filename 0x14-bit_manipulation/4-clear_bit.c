93b7c122dc3d93d43032a6faa963607b3cc9e47e
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-28 13:54:35 -0700

    task 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,14 +7,15 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8)
-		{
-		return (-1);
-		}
-	unsigned long int bit;
+	unsigned int m;
 
-	bit = 1 << index;
-	if (*n & bit)
-		*n ^= bit;
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
 	return (1);
 }
