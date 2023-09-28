0db5c5a3d4ee670de5197dc2fbfe0d26faf84f2d
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-28 11:37:34 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,9 +8,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int number = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (*b != NULL)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 		{
