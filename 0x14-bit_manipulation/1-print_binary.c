2452b736850e715a1bc268402e74a9220aeb71f9
HadeerAdelouf <hadiradel999@gmail.com>
2023-09-28 12:22:37 -0700

    task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,13 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int bit;
 	int shift = sizeof(unsigned long int) * 8 - 1;
 	int flag = 0;
 
 	while (shift >= 0)
 	{
-		unsigned long int bit = n >> shift;
+		bit = n >> shift;
 
 		if (bit & 1)
 		{
