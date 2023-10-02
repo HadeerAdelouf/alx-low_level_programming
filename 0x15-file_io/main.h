b19a07dabe3caf522639a74e7bc6a8cb7cac329b
HadeerAdelouf <hadiradel999@gmail.com>
2023-10-02 14:24:21 -0700

    task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,22 +8,11 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #define BUFFER_SIZE 1024
-/**
- * print_error_exit - prints error messg
- * @code:number of exit
- * @message:message to print
- * Return:code number
- */
-void print_error_exit(int code, const char *message)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
-	exit(code);
-}
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void print_error_exit(int code, const char *message);
 
 #endif
 
