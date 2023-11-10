d3805a12e2ec29f45a6264b67c66941e9246807c
HadeerAdelouf <hadiradel999@gmail.com>
2023-11-10 23:50:51 +0200

    6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+nclude "lists.h"
+/**
+ * sum_dlistint - show us the sum of all the data (n) in a dLL
+ * @head: a pointer to the head of the dLL
+ * Return: if empty, return 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+		int add_sum = 0;
+
+			while (head != NULL)
+			{
+				add_sum += head->n;
+				head = head->next;
+			}
+			return (add_sum);
+}
